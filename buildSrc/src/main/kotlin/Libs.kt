8bbd9d2ed699f5b85eec7929de5b59c409d187af
Juan Cordero Ordóñez <juancorderoordonez@gmail.com>
2022-09-23 17:23:16 +0200

    Initial commit

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,41 @@
+object Libs {
+    val androidLibs = listOf(
+        "androidx.core:core-ktx:${Versions.core}",
+        "androidx.appcompat:appcompat:${Versions.appCompat}",
+        "com.google.android.material:material:${Versions.material}",
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifeCycle}",
+        "com.google.dagger:hilt-android:${Versions.hilt}",
+        "androidx.recyclerview:recyclerview:${Versions.recyclerView}",
+        "androidx.room:room-runtime:${Versions.room}",
+        "androidx.room:room-ktx:${Versions.room}"
+    )
+
+    val androidKaptLibs = listOf(
+        "com.google.dagger:hilt-compiler:${Versions.hilt}",
+        "androidx.room:room-compiler:${Versions.room}",
+        "com.github.bumptech.glide:compiler:${Versions.glide}"
+    )
+
+    val kotlinLibs = listOf(
+        "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}",
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}",
+        "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}",
+    )
+
+    val libs = listOf(
+        "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}",
+        "com.squareup.retrofit2:retrofit:${Versions.retrofit}",
+        "com.squareup.retrofit2:converter-gson:${Versions.retrofit}",
+        "com.github.bumptech.glide:glide:${Versions.glide}",
+        "androidx.cardview:cardview:1.0.0"
+    )
+
+    val testLibs = listOf(
+        "junit:junit:${Versions.junit}"
+    )
+
+    val androidTestLibs = listOf(
+        "androidx.test.espresso:espresso-contrib:${Versions.espresso}",
+        "androidx.test.ext:junit-ktx:${Versions.junitKtx}",
+    )
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val appCompat = "1.5.0"
+    const val core = "1.8.0"
+    const val coroutines = "1.6.4"
+    const val hilt = "2.38.1"
+    const val junit = "4.13.2"
+    const val junitKtx = "1.1.3"
+    const val kotlin = "1.7.10"
+    const val lifeCycle = "2.5.1"
+    const val material = "1.6.1"
+    const val okhttp = "4.9.1"
+    const val retrofit = "2.9.0"
+    const val recyclerView = "1.2.1"
+    const val room = "2.4.3"
+    const val espresso = "3.4.0"
+    const val glide = "4.13.2"
+}
